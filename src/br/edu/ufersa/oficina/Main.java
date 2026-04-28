dbb1f61b3a74492430abad4263845ce99988cb96
José Roberto <joseroberto2k05@gmail.com>
2026-04-27 22:02:43 -0300

    Tests
    Com o banco de dados populado, a consulta e a inicialização dos objetos foi realizada com sucesso

--- src/br/edu/ufersa/oficina/Main.java
+++ src/br/edu/ufersa/oficina/Main.java
@@ -1,8 +1,6 @@
 package br.edu.ufersa.oficina;
 
-import br.edu.ufersa.oficina.DAO.BudgetDAO;
-import br.edu.ufersa.oficina.DAO.OrderDAO;
-import br.edu.ufersa.oficina.DAO.UserDAO;
+import br.edu.ufersa.oficina.DAO.*;
 import br.edu.ufersa.oficina.connection.ConnectionDB;
 import br.edu.ufersa.oficina.entity.*;
 
@@ -11,8 +9,30 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        CarDAO cd = new CarDAO();
+        PartsDAO pd = new PartsDAO();
+
+        for (Car c: cd.getAllCar())
+            c.show();
+
+        for (Parts p: pd.getAllParts())
+            p.show();
+
+        ClientDAO cld = new ClientDAO();
+        ServiceDAO sd = new ServiceDAO();
+
+        for (Client c: cld.getAllClient())
+            c.show();
+
+        for (Service s: sd.getAllService())
+            s.show();
+
+        UserDAO ud = new UserDAO();
         BudgetDAO bd = new BudgetDAO();
         OrderDAO od = new OrderDAO();
+
+        for (User u: ud.getAllUsers())
+            u.show();
 
         for (Budget b: bd.getAllBudget())
             b.show();
