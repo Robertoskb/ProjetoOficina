43a3d3006a1cb8268d2019a47177916639aba227
José Roberto <joseroberto2k05@gmail.com>
2026-06-22 16:53:51 -0300

    Update TransactionObserver.java

--- src/main/java/br/edu/ufersa/oficina/controller/Paginator/TransactionObserver.java
+++ src/main/java/br/edu/ufersa/oficina/controller/Paginator/TransactionObserver.java
@@ -1,7 +1,5 @@
 package br.edu.ufersa.oficina.controller.Paginator;
 
 public interface TransactionObserver extends Observer {
-    void delete(int id);
-    void edit(int id);
     void finish(int id);
 }
