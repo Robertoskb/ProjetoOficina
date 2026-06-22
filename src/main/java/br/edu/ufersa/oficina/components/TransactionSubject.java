5210c141d2ecec8996793f0888c4a8e60d8d1147
José Roberto <joseroberto2k05@gmail.com>
2026-06-22 18:37:31 -0300

    Update TransactionSubject.java

--- src/main/java/br/edu/ufersa/oficina/components/TransactionSubject.java
+++ src/main/java/br/edu/ufersa/oficina/components/TransactionSubject.java
@@ -3,11 +3,9 @@ package br.edu.ufersa.oficina.components;
 import br.edu.ufersa.oficina.controller.Paginator.TransactionObserver;
 
 public interface TransactionSubject extends Subject {
-    public void registerObserver(TransactionObserver observer);
+    void registerObserver(TransactionObserver observer);
 
-    public void removeObserver(TransactionObserver observer);
+    void removeObserver(TransactionObserver observer);
 
-    public void notifyDelete(int id);
-    public void notifyEdit(int id);
     public void notifyFinish(int id);
 }
